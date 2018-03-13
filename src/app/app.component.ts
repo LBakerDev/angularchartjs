91c186aa3fef0254d4c3119bd04c33b0f02aed32
londonbaker4@gmail.com <londonbaker4@gmail.com>
2018-03-12 20:39:30 -0400

    chart.js work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { WeatherService } from "./weather.service";
+import { Chart } from "chart.js";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  chart = [];
+  constructor(private weather: WeatherService) {}
+
+  ngOnInit() {
+    this.weather.dailyForecast().subscribe(res => {
+      let tempMax = res["list"].map(res => res.main.temp_max);
+      let tempMin = res["list"].map(res => res.main.temp_min);
+      let allDates = res["list"].map(res => res.dt);
+
+      let weatherDates = [];
+      allDates.forEach(element => {
+        let jsDate = new Date(element * 1000);
+        weatherDates.push(
+          jsDate.toLocaleTimeString("en", {
+            year: "numeric",
+            month: "short",
+            day: "numeric"
+          })
+        );
+      });
+      this.chart = new Chart("canvas", {
+        type: "line",
+        data: {
+          labels: weatherDates,
+          datasets: [
+            {
+              data: tempMax,
+              borderColor: "#3cba9f",
+              fill: false
+            },
+            {
+              data: tempMin,
+              borderColor: "#ffcc00",
+              fill: false
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [
+              {
+                display: true
+              }
+            ],
+            yAxes: [
+              {
+                display: true
+              }
+            ]
+          }
+        }
+      });
+    });
+  }
 }
